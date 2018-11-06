e637bbc013d4af0b2e06103786eb1657a4ce03b3
D3473R <d3473r@gmail.com>
2018-11-06 21:24:28 +0100

    Add weather publisher

---
+++ src/weather.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os
+import sys
+import json
+import curses
+import random
+import locale
+import collections
+import RPi.GPIO as GPIO
+import paho.mqtt.publish as publish
+
+from time import sleep
+from asciichartpy import plot
+from gpiozero import MCP3008
+from datetime import datetime
+
+__author__ = "Fabian Wurster"
+__license__ = "MIT"
+__version__ = "1.0"
+__email__ = "fwurster@stud.hs-heilbronn.de"
+
+
+MQTT_SERVER = 'localhost'
+MQTT_PATH = 'weather'
+
+SPEED_PIN = 25
+DIRECTION_ERROR = 359
+DIRECTION_PRECISION = 0.015
+SEND_SLEEP = 0.2
+
+SPEED_KMH = 2.5
+SPEED_MS = SPEED_KMH / 3.6
+
+DIRECTIONS = {3.84: 0, 1.98: 22.5, 2.25: 45, 0.41: 67.5, 0.45: 90, 0.32: 112.5, 0.9: 135, 0.62: 157.5, 1.4: 180, 1.19: 202.5, 3.08: 225, 2.93: 247.5, 4.62: 270, 4.04: 292.5, 4.33: 315, 3.43: 337.5}
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SPEED_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+ENABLE_GUI = True
+
+locale.setlocale(locale.LC_ALL, 'C.UTF-8')
+
+rows, columns = os.popen('stty size', 'r').read().split()
+rows = int(rows)
+columns = int(columns)
+
+plot_width = int(columns * 0.8)
+tmp = MCP3008(channel=0, device=0)
+
+last_speed_timestamp = datetime.now()
+speed = 0.0
+
+def speed_switch(channel):
+    global speed
+    global last_speed_timestamp
+    now = datetime.now()
+    seconds_since_timestamp = (now - last_speed_timestamp).total_seconds()
+    speed = SPEED_MS * (1 / seconds_since_timestamp)
+    last_speed_timestamp = now
+
+GPIO.add_event_detect(SPEED_PIN, GPIO.RISING, callback=speed_switch)
+
+def gui(scr):
+    scr.clear()
+    deque = collections.deque([0] * plot_width, maxlen=plot_width)
+
+    while(True):
+    	value = tmp.value * 5.0
+        direction = map_direction(value)
+        deque.popleft()
+        deque.append(value)
+        scr.addstr(0, 0, plot(deque, {'minimum': 0.0, 'maximum': 5.0}))
+        scr.refresh()
+        publish.single(MQTT_PATH, build_json_package(direction), hostname=MQTT_SERVER)
+        sleep(SEND_SLEEP)
+
+def headless():
+    while(True):
+    	value = tmp.value * 5.0
+        direction = map_direction(value)
+        publish.single(MQTT_PATH, build_json_package(direction), hostname=MQTT_SERVER)
+        sleep(SEND_SLEEP)
+
+def map_direction(direction_voltage):
+    for voltage in DIRECTIONS:
+        if abs(voltage - direction_voltage) < DIRECTION_PRECISION:
+            return DIRECTIONS.get(voltage)
+    return DIRECTION_ERROR
+
+def build_json_package(direction):
+    return json.dumps({'speed': speed, 'direction': direction, 'timestamp': datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')})
+
+if __name__ == '__main__':
+    try:
+        if ENABLE_GUI:
+            curses.wrapper(gui)
+        else:
+            headless()
+    except KeyboardInterrupt:
+        try:
+            GPIO.cleanup()
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
